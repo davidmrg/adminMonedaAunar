7ae3b384c0383a95581bbe3383bfd97e1c7d931a
David M Ramirez <davidramirez@MacBook-Pro-de-David.local>
2020-11-28 09:58:20 -0500

    make it better

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var router = require('express').Router();
-const { requiresAuth } = require('express-openid-connect');
+// const { requiresAuth } = require('express-openid-connect');
 const dataQuery = require('../db/conectionDB');
 
 
@@ -11,61 +11,19 @@ router.get('/', function(req, res, next) {
         res.render('index', {
             title: 'Moneda Aunar',
             data: data,
-            isAuthenticated: req.oidc.isAuthenticated()
+            
         });
     }).catch((err) => console.log(err));
 });
 
-router.get('/profile', requiresAuth(), function(req, res, next) {
-    const elementActividades = {}
-        // capturar email del Auth0 para usarlo en el query
-    console.log(req.oidc.user.email);
-    const mail = req.oidc.user.email;
-    const dataQ = dataQuery.resultQuery(`SELECT estudiante.puntos_acum ,estudiante.nombre ,estudiante.numidentidad,estudiante.carrera,estudiante.apellido,actividad.desc_actividad ,actividad.puntos_actividad, participacion.fecha_participacion from estudiante inner join participacion on participacion.id_estudiante =estudiante.id_estudiante
-    inner join actividad on actividad.id_actividad = participacion.id_actividad WHERE estudiante.email= '${mail}' ;`).then((data) => {
-            // const dataQ = dataQuery.resultQuery(`SELECT * from estudiante ;`).then((data) => {
-            console.log(data);
-            // stringify recibe el data y lo transforma en un json de tipo string
-            let datos = JSON.stringify(data, null, 2);
 
-            // convertir de string a json:
-            var datos2 = JSON.parse(datos);
-            for (const key in datos2) {
-                if (datos2.hasOwnProperty(key)) {
-                    elementActividades[`Actividad${key}`] = datos2[key].desc_actividad;
-                    elementActividades[`Puntos${key}`] = datos2[key].puntos_actividad;
-
-                }
-            }
-            console.log('const', elementActividades);
-
-            // console.log('los datos son');
-            // console.log(datos2);
-            // capturar solo nombre
-            // console.log('nombre: ');
-            // console.log(datos2[0].nombre);
-            res.render('profile', {
-                userProfile: JSON.stringify(req.oidc.user, null, 2),
-                title: 'Perfil',
-                nombre: datos2[0].nombre,
-                apellido: datos2[0].apellido,
-                numIdentidad: datos2[0].numidentidad,
-                carrera: datos2[0].carrera,
-                puntos_Acum: datos2[0].puntos_acum,
-                actividad: datos2[0].desc_actividad,
-                fecha: datos2[0].fecha_participacion,
-                puntosActividad: datos2[0].puntos_actividad
-            });
-        })
-        .catch((err) => console.log(err));
-});
 
 router.get('/registro', (req, res) => {
-    res.render('admin/registro',);
+    res.render('admin/registro');
   });
 
   router.get('/registro2', (req, res) => {
-    res.render('admin/prueba',);
+    res.render('admin/prueba');
   });
 
 
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 const express = require('express');
 const http = require('http');
 const logger = require('morgan');
 const path = require('path');
 const router = require('./routes/index');
-const { auth } = require('express-openid-connect');
+// const { auth } = require('express-openid-connect');
 
-dotenv.load();
+// dotenv.load();
 
 const app = express();
 
@@ -18,43 +18,22 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-const config = {
-  authRequired: false,
-  auth0Logout: true
-};
 
 const port = process.env.PORT || 3000;
-if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
+/* if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
   config.baseURL = `http://localhost:${port}`;
-}
+} */
 
-app.use(auth(config));
+// app.use(auth(config));
 
-// Middleware to make the `user` object available for all views
-app.use(function (req, res, next) {
-  res.locals.user = req.oidc.user;
-  next();
-});
+
 
 app.use('/', router);
 
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// Error handlers
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: process.env.NODE_ENV !== 'production' ? err : {}
-  });
-});
+
 
 http.createServer(app)
   .listen(port, () => {
-    console.log(`Listening on ${config.baseURL}`);
+    console.log(`Listening on ${port}`);
   });
